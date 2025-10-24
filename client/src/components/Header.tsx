07760223d2b0793b1121bcfc5d6a1660c0240cda
Samuel_OMAGO <sammuaraujo200@gmail.com>
2025-10-24 11:52:33 -0300

    Centralizando logo corretamente

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -60,13 +60,14 @@ export default function Header({ theme = "default" }: HeaderProps) {
   </div>
 
   {/* LOGO CENTRAL */}
-  <div className="flex-shrink-0 flex items-center justify-center mx-10">
-    <img
-      src="/images/cine-estação_1761215287537.png"
-      alt="Cine Estação"
-      className="h-14 w-auto"
-    />
-  </div>
+<div className="flex justify-center items-center w-full">
+  <img
+    src="/images/cine-estação_1761215287537.png"
+    alt="Cine Estação"
+    className="h-14 w-auto mx-auto"
+  />
+</div>
+
 
   {/* Botões da direita */}
   <div className="hidden md:flex items-center justify-start gap-10 flex-1">
